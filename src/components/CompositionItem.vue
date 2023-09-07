3c75929b48e3585c9752de60e08c76f3cb6f35b6
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-09-07 11:10:34 +0700

    Add upload cancellation and prevent upload on navigating away from the Manage page

---
+++ src/components/CompositionItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- Composition Items -->
+  <div class="border border-gray-200 p-3 mb-4 rounded">
+    <div v-show="!showForm">
+      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
+      <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
+        <i class="fa fa-times"></i>
+      </button>
+      <button
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right"
+        @click.prevent="showForm = !showForm"
+      >
+        <i class="fa fa-pencil-alt"></i>
+      </button>
+    </div>
+    <div v-show="showForm">
+      <VeeForm :validation-schema="schema">
+        <div class="mb-3">
+          <label class="inline-block mb-2">Song Title</label>
+          <VeeField
+            name="modified_name"
+            type="text"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Song Title"
+            :value="song.modified_name"
+          />
+        </div>
+        <div class="mb-3">
+          <label class="inline-block mb-2">Genre</label>
+          <VeeField
+            name="genre"
+            type="text"
+            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+            placeholder="Enter Genre"
+          />
+        </div>
+        <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">Submit</button>
+        <button
+          type="button"
+          class="py-1.5 px-3 rounded text-white bg-gray-600"
+          @click.prevent="showForm = !showForm"
+        >
+          Go Back
+        </button>
+      </VeeForm>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompositionItem',
+  props: {
+    song: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      showForm: false,
+      schema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces'
+      }
+    }
+  }
+}
+</script>
--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -3,7 +3,7 @@
   <section class="container mx-auto mt-6">
     <div class="md:grid md:grid-cols-3 md:gap-4">
       <div class="col-span-1">
-        <AppUpload />
+        <AppUpload ref="upload" />
       </div>
       <div class="col-span-2">
         <div class="bg-white rounded border border-gray-200 relative flex flex-col">
@@ -12,88 +12,7 @@
             <i class="fa fa-compact-disc float-right text-green-400 text-2xl"></i>
           </div>
           <div class="p-6">
-            <!-- Composition Items -->
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-              <div>
-                <form>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Song Title</label>
-                    <input
-                      type="text"
-                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Song Title"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label class="inline-block mb-2">Genre</label>
-                    <input
-                      type="text"
-                      class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                      placeholder="Enter Genre"
-                    />
-                  </div>
-                  <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
-                    Submit
-                  </button>
-                  <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
-                    Go Back
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
-            <div class="border border-gray-200 p-3 mb-4 rounded">
-              <div>
-                <h4 class="inline-block text-2xl font-bold">Song Name</h4>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
-                  <i class="fa fa-times"></i>
-                </button>
-                <button class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
-                  <i class="fa fa-pencil-alt"></i>
-                </button>
-              </div>
-            </div>
+            <CompositionItem v-for="song in songs" :key="song.docID" :song="song" />
           </div>
         </div>
       </div>
@@ -104,12 +23,39 @@
 <script>
 // import useUserStore from '@/stores/user'
 import AppUpload from '@/components/Upload.vue'
+import CompositionItem from '@/components/CompositionItem.vue'
+import { songsCollection, auth } from '@/includes/firebase'
 
 export default {
   name: 'manage',
   components: {
-    AppUpload
+    AppUpload,
+    CompositionItem
+  },
+  async created() {
+    // Generate a query for fetching data
+    // .get() executes the query and returns a snapshot of the data
+    const snapshot = await songsCollection.where('uid', '==', auth.currentUser.uid).get()
+
+    snapshot.forEach((document) => {
+      const song = {
+        ...document.data(), // .data() gets the raw data but not the ID
+        docID: document.id
+      }
+
+      this.songs.push(song)
+    })
+  },
+  data() {
+    return {
+      songs: []
+    }
   }
+  // beforeRouteLeave(to, from, next) {
+  //   this.$refs.upload.cancelUploads()
+
+  //   next()
+  // }
   // beforeRouteEnter(to, from, next) {
   //   const store = useUserStore()
 
