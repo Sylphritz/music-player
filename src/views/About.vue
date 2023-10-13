feca012678b25f5124f485d3dd43b40c5be4d438
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-10-13 21:44:26 +0700

    Prevent crawling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,5 @@
 <template>
-  <div>About</div>
+  <div class="container mx-auto py-4 text-white text-center">
+    <h1 class="text-4xl font-thin uppercase tracking-wider">About</h1>
+  </div>
 </template>
