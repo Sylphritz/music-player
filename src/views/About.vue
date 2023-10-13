458946ebd486ea72fa253a52b807dd87c239493f
Sylphritz <3182197+Sylphritz@users.noreply.github.com>
2023-10-13 21:51:47 +0700

    Add the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
-  <div class="container mx-auto py-4 text-white text-center">
-    <h1 class="text-4xl font-thin uppercase tracking-wider">About</h1>
+  <div class="container mx-auto py-4 text-white">
+    <h1 class="text-4xl font-thin uppercase tracking-wider mb-6 text-center">About</h1>
+
+    <p class="w-[720px] max-w-full mx-auto px-4 mb-4">
+      Hi, I'm <b>Pavee Udomkarnpaisarn</b>. A Vue.js developer. This Vue.js application was made
+      with the intention of showcasing my various skills related to full-stack app development.
+    </p>
+
+    <p class="w-[720px] max-w-full mx-auto px-4 mb-4">
+      The source code for this project is available publicly on
+      <a
+        class="font-bold text-pink-400 hover:text-pink-500"
+        href="https://github.com/Sylphritz/music-player"
+        target="_blank"
+        >GitHub</a
+      >. Feel free to take a look.
+    </p>
+
+    <p class="w-[720px] max-w-full mx-auto px-4 mb-4">
+      Also, checkout my portfolio at
+      <a
+        class="font-bold text-pink-400 hover:text-pink-500"
+        href="https://pavee.dev"
+        target="_blank"
+        >Pavee.dev</a
+      >
+    </p>
   </div>
 </template>
